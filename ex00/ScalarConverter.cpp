c847f420f099d37e3cc707b63c5f12617b857d81
Anna Kwadrans <annakwadrans@MacBook-Air-de-Anna.local>
2026-06-21 22:45:19 +0200

    conv from char

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -5,28 +5,100 @@ ScalarConverter::ScalarConverter(const ScalarConverter &other) { (void)other; }
 ScalarConverter::~ScalarConverter() {}
 ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other) { (void)other; return(*this);}
 
-//static Type getType(const std::string &str) {
-//    (void)str;
-//    return (INT);
-//}
+
+
+static Type getType(const std::string &str)
+{
+    if ((str.length() == 1 && !isdigit(str.front())) ||
+            (str.length() == 3 && str.front() == '\'' && str.back() == '\''))
+        return (CHAR);
+    else if (str.find('.') == std::string::npos)
+        return (INT);
+    else if (str.back() == 'f')
+        return (FLOAT);
+    else
+        return (DOUBLE);
+}
+
+static void     convertChar(const std::string &str) {
+    
+    char    c;
+
+    if (str.length() == 1) {
+        c = *str.begin();
+    }
+    else if (str.length() == 3)
+        c = (str[str.find_first_not_of('\'')]);
+    else {
+        std::cout << "CAHR NOT VALID" << std::endl;
+        return ;
+    }
+
+    std::cout << "char: " << c << std::endl
+        << "int: " << static_cast<int>(c) << std::endl
+        << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(c) << "f" << std::endl
+        << "double: " << static_cast<double>(c) << std::endl;
+    /*
+    int integer;
+    std::stringstream ss;
+
+    switch (type)
+    {
+        case CHAR:
+            if (str.length() == 1)
+                return (static_cast<char>(*str.begin()));
+            else if (str.length() == 3)
+                return (static_cast<char>(str[str.find_first_not_of('\'')]));
+        case INT:
+            ss << str;
+            ss >> integer;
+
+            //if (integer < 32 || integer > 126)
+                // char: Non displayable
+            return ('a');
+        case FLOAT:
+            return ('a');
+        case DOUBLE:
+            return ('a');
+    }
+    
+    if (str.length() == 1)
+        return (static_cast<char>(*str.begin()));
+    else if*/ 
+    //return ('a');
+}
+
+static void      convertInt(const std::string &str) {
+    (void)str;
+}
+
+static void    convertFloat(const std::string &str) {
+    (void)str;
+}
+
+static void   convertDouble(const std::string &str) {
+    (void)str;
+}
 
 void ScalarConverter::convert(const std::string &str) {
 
-    //char    c;
-    //int     i;
-    //float   f;
-    //double  d;
+    // verify
     
     // data type
-    //Type type = getType(str);
+    Type type = getType(str);
 
-    // convert
-    int i = atoi(str.c_str());
+    //convert & print
+    switch (type)
+    {
+        case CHAR:
+            convertChar(str);
+        case INT:
+            convertInt(str);
+        case FLOAT:
+            convertFloat(str);
+        case DOUBLE:
+            convertDouble(str);
+    }
 
-    std::cout //<< "char: " << c << std::endl
-        << "int: " << i << std::endl
-        /*<< "float: " << f << std::endl
-        << "double: " << d << std::endl*/;
-    
     return;
 }
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -4,8 +4,10 @@
 # include <string>
 # include <iostream>
 # include <cstdlib>
+# include <sstream>
+# include <iomanip>
 
-typedef enum Type {
+typedef enum eType {
     CHAR,
     INT,
     FLOAT,
@@ -14,13 +16,17 @@ typedef enum Type {
 
 class ScalarConverter
 {
-    public:
+    private:
+
         ScalarConverter();
         ScalarConverter(const ScalarConverter &other);
         ~ScalarConverter();
         ScalarConverter &operator=(const ScalarConverter &other);
+    
+    public:
 
         static void convert(const std::string &str);
+
 };
 
 #endif
